672f8503f48a5a9e69fdcdf4d7c272506f320444
Arthur Florenço <135295547+arthurflorenco@users.noreply.github.com>
2024-05-05 18:57:45 +0100

    first commit

---
+++ sucessom.js
@@ -0,0 +1,8 @@
+//Animations
+
+gsap.from("#sucessom", { opacity: -1, duration: 1.5, delay: 1, x: -30 });
+gsap.from("#eletronicos", { opacity: -1, duration: 1.5, delay: 1, x:100 });
+gsap.from("#informatica", { opacity: -1, duration: 1.5, delay: 1, x: -30 });
+gsap.from("#frase", { opacity: -1, duration: 2.5, delay: 1 });
+gsap.from(".social-media", { opacity: -1, duration: 2.5, delay: 1 });
+
